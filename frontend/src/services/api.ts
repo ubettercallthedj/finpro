d65e51766bf19ec646839d0ede104813fc6fe7f3
bettercallthedj <ubettercallthedj@gmail.com>
2026-01-05 17:10:58 -0300

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -3,6 +3,7 @@ import toast from 'react-hot-toast'
 
 const api = axios.create({
   baseURL: '/api',
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
