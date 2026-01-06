8aa1b748859d6b44df3c7affa9710c4afffae112
bettercallthedj <ubettercallthedj@gmail.com>
2026-01-05 21:32:17 -0300

    Update AllControllers.php

--- backend/app/Http/Controllers/Api/AllControllers.php
+++ backend/app/Http/Controllers/Api/AllControllers.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use App\Models\User;
 use Carbon\Carbon;
 
 // ========================================
